3836418a7a3b4b953d4c6f2a0006fcd94d35eea4
Igor <igordomanskii0896@gmail.com>
2023-11-13 17:47:51 +1000

    user
    get auth user data

---
+++ api/src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) {}
+
+	async byId(id: number) {
+		const user = await this.prisma.user.findUniqueOrThrow({
+			where: {
+				id: Number(id)
+			},
+			select: {
+				id: true,
+				username: true,
+				password: false,
+				tasks: true
+			}
+		})
+
+		return user
+	}
+}
